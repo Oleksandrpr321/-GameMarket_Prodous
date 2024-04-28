1a5e1282c5db54db1623fcaf824e6d5bbf97d356
Prodous Oleksandr <158211662+Oleksandrpr321@users.noreply.github.com>
2024-04-28 16:51:37 +0300

    зірки

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log(1);
+
+document.addEventListener("DOMContentLoaded", function() {
+    const stars = document.querySelectorAll(".star");
+  
+    stars.forEach(function(star) {
+      star.addEventListener("click", function() {
+        const value = parseInt(star.getAttribute("data-value"));
+        resetStars();
+        for (let i = 0; i < value; i++) {
+          stars[i].classList.add("active");
+        }
+      });
+    });
+  
+    function resetStars() {
+      stars.forEach(function(star) {
+        star.classList.remove("active");
+      });
+    }
+  });
+  
