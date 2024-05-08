642223bcb6de47a5221b602aebdb509926890568
Prodous Oleksandr <158211662+Oleksandrpr321@users.noreply.github.com>
2024-05-08 14:47:56 +0300

    /

--- market.js
+++ market.js
@@ -4,18 +4,35 @@ console.log('Перевірка чи підключено market.js')
 let itemsDiv = document.getElementById("items");
 //Перевірка існування блоку
 if (itemsDiv) {
-//Вивід знайденого елементу
+    //Вивід знайденого елементу
     console.log(itemsDiv)
-//Додавання відформатованого HTML коду в блок
-// itemsDiv.innerHTML += '<div class = "item"></div>'
-// itemsDiv.innerHTML += '<div class = "item"></div>'
-// itemsDiv.innerHTML += '<div class = "item"></div>'
-// itemsDiv.innerHTML += '<div class = "item"></div>'
-for (let i= 0; i < 100; i++){
-    itemsDiv.innerHTML += '<div class = "item"></div>'
-}
+    //Додавання відформатованого HTML коду в блок
+    // itemsDiv.innerHTML += '<div class = "item"></div>'
+    // itemsDiv.innerHTML += '<div class = "item"></div>'
+    // itemsDiv.innerHTML += '<div class = "item"></div>'
+    // itemsDiv.innerHTML += '<div class = "item"></div>'
+    for (let i = 0; i < 100; i++) {
+        itemsDiv.innerHTML += '<div class = "item"></div>'
+    }
 } else {
     //Повідомлення про не знайдений блок
-console.log('Блок товарів не знайдено')
+    console.log('Блок товарів не знайдено')
 
+}
+let itemArray = [
+    'Газонокосарка 43',
+    'Електричний тример 110',
+    'Електрична газонокосарка 32',
+    'Акумуляторний оприскувач 12 N',
+    'Газокосарка 430',
+    'Електричний тример 130',
+    'Електрична газонокосарка 320',
+'Акумуляторний оприскувач 12 E',
+
+
+]
+console.log(itemArray)
+
+for (let i = 0; i < itemArray.itemArray.length; i++) {
+    console.log(itemArray[i])
 }
